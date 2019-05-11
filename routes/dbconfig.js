8c3fb421dd395fd23dc045efce5283ff170c15ba
myeonghuSong <21300390@handong.edu>
2019-05-12 01:20:44 +0900

    Create dbconfig.js

---
+++ routes/dbconfig.js
@@ -0,0 +1,15 @@
+module.exports =
+// {
+//     host: '10.0.0.1',
+//     user: 'audgn0811',
+//     password: 'audgn811',
+//     password: '****',
+//     database: 'audgn0811'
+//
+// };
+module.exports =
+    user: 'root',
+    password: '1234',
+    database: 'boardDB'
+}; 
+};
